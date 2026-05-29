718aab65dda6eca7bae54eca535284a24023000a
ErnestoMLopez <ernesto.lopez@ing.unlp.edu.ar>
2026-05-29 12:14:42 -0300

    Added doxy documentation

--- c/src/viterbi.h
+++ c/src/viterbi.h
@@ -21,7 +21,15 @@
  * =======================================================================
  */
 
-#define VBD_MAX_DECODERS         6
+/**
+ * @brief Maximum number of Viterbi decoder instances that can be initialized simultaneously.
+ */
+#define VBD_MAX_DECODERS 6
+
+/**
+ * @brief Maximum number of output symbols per step (n) supported by the decoder. This limit is needed only to determine
+ * the memory asigned to store the output generators.
+ */
 #define VBD_MAX_SYMBOLS_PER_STEP 8
 
 
@@ -30,12 +38,22 @@
  * =======================================================================
  */
 
-typedef struct v_generator {
-    uint32_t poly;    //< Generator polynomial (LSB is the most recent bit)
-    bool isInverted;  //< Indicates if the output branch is inverted
-} v_generator_t;
-
+/**
+ * @brief Control structure for a Viterbi block decoder instance.
+ */
 typedef struct vbd_ctrl vbd_ctrl_t;
+
+/**
+ * @brief Data type for the generator polynomials used in the convolutional encoder.
+ *
+ * The generator polynomial is represented as a 32-bit unsigned integer, where the least significant bit (LSB)
+ * corresponds to the most recent bit in the shift register. The `isInverted` flag indicates whether the branch
+ * corresponding to this generator is inverted or not to generate the output symbol.
+ */
+typedef struct v_generator {
+    uint32_t poly;
+    bool isInverted;
+} v_generator_t;
 
 
 /* =======================================================================
@@ -50,6 +68,22 @@ typedef struct vbd_ctrl vbd_ctrl_t;
  * =======================================================================
  */
 
+/**
+ * @brief Initializes a Viterbi block decoder instance.
+ *
+ * @param vbdCtrl Pointer to a pointer that will hold the control structure for the decoder instance.
+ * @param symbolsPerInput Total number of symbols that are input to the decoder for each block.
+ * @param bitsPerStep Parameter k = Number of bits input to the encoder for each step.
+ * @param symbolsPerStep Parameter n = Number of symbols output by the encoder for each step.
+ * @param constraintLength Parameter K = Constraint length of the convolutional code.
+ * @param symbolGenerators Pointer to an array of ::symbolsPerStep generator polynomials.
+ * @param workingBuffer Buffer needed for the survivors path and metrics storage.
+ * @param bufferSize Buffer size [bytes].
+ * @return 0  = success.
+ *         -1 = maximum decoders instances limit reached.
+ *         -2 = too many symbols per step.
+ *         -3 = not enough workspace memory.
+ */
 int32_t viterbiBlockDecoderInit(
         vbd_ctrl_t** vbdCtrl,
         const uint8_t symbolsPerInput,
@@ -60,6 +94,15 @@ int32_t viterbiBlockDecoderInit(
         const uint8_t* workingBuffer,
         const uint32_t bufferSize);
 
+/**
+ * @brief Decodes a block of symbols using the Viterbi algorithm.
+ *
+ * @param vbdCtrl Pointer to the control structure for the decoder instance.
+ * @param in Input buffer containing the symbols to be decoded.
+ * @param out Output buffer where the decoded bits will be stored.
+ * @return -1 = invalid arguments.
+ *        >=0 = number of bit errors detected and corrected.
+ */
 int32_t viterbiBlockDecoder(const vbd_ctrl_t* vbdCtrl, const uint8_t* in, uint8_t* out);
 
 #endif
